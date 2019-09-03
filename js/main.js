38523f0ec3ee51816291a31d45b641ca62f041bc
egeilgaz <ilgaz-ege@hotmail.com>
2019-09-03 14:54:23 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -192,3 +192,31 @@ var petit = document.getElementById('petit');
 petit.onclick = function() {
   window.alert('Yakında Petit Princess Cakery sayfamız hizmetinizde olacaktır');
 };
+
+/*mobile için hamburger menü
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu3');
+const menuNav = document.querySelector('.menu2');
+const navItem = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+  }
+}*/
